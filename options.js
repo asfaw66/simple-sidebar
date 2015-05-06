bd77ef956142bbd928b25b72a33da73702998b3e
Davide DC <dcdeiv@users.noreply.github.com>
2015-05-07 00:07:01 +0200

    Update options.js

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 /*
-** Simple Sidebar v1.2.0 by DcDeiv https://github.com/dcdeiv
+** Simple Sidebar v1.2.1 by DcDeiv https://github.com/dcdeiv
 **
 ** GPLv2 http://www.gnu.org/licenses/gpl-2.0-standalone.html
 **
