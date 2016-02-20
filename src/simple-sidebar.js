ce303e5b60843180363a874fcf022296cbbf409c
Davide Di Criscito <davide.dicriscito@gmail.com>
2016-02-20 11:19:39 +0100

    fix indentation

--- src/simple-sidebar.js
+++ src/simple-sidebar.js
@@ -3,38 +3,38 @@
 	"use strict";
 
 		var pluginName = "simpleSidebar",
-				defaults = {
-				propertyName: "value"
+			defaults = {
+			propertyName: "value"
 		};
 
 		function Plugin( element, options ) {
-				this.element = element;
-				this.settings = $.extend( {}, defaults, options );
-				this._defaults = defaults;
-				this._name = pluginName;
-				this.init();
+			this.element = element;
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
 		}
 
 		$.extend(Plugin.prototype, {
-				init: function() {
-					var cfg = this.settings;
+			init: function() {
+				var cfg = this.settings;
 
-					this.echo( cfg );
-				},
-				echo: function( e ) {
-					console.log( e );
-				}
+				this.echo( cfg );
+			},
+			echo: function( e ) {
+				console.log( e );
+			}
 		});
 
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
 		$.fn[ pluginName ] = function( options ) {
-				return this.each( function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" +
-									pluginName, new Plugin( this, options ) );
-						}
-				});
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Plugin( this, options ) );
+				}
+			});
 		};
 
 } )( jQuery, window, document );
